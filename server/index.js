a70ccc21094007b0fa6a455fc71d0aa365b09324
jameem <jameemkurikkal.mp@gmail.com>
2020-03-17 08:37:45 +0530

    init google oauth

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').strategy
-
+const keys = require('./config/keys')
 
 const app = express()
 
-passport.use(mew GoogleStrategy())
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientId,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: 'auth/google/callback'
+}, (accessToken) => {
+    console.log(accessToken)
+}))
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.json({
         hi: "hahaha"
     })
@@ -16,4 +22,3 @@ app.get('/', (req,res)=>{
 app.listen('5000')
 
 // http://localhost:5000/auth/google/callback
-
