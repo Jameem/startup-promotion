1ebe2886f1f84ff0aa7f16e3d2dfcb8fe9d4a361
jameem <jameemkurikkal.mp@gmail.com>
2020-03-17 08:18:40 +0530

    setting up passport

--- server/index.js
+++ server/index.js
@@ -1,5 +1,11 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').strategy
+
+
 const app = express()
+
+passport.use(mew GoogleStrategy())
 
 app.get('/', (req,res)=>{
     res.json({
@@ -8,3 +14,8 @@ app.get('/', (req,res)=>{
 })
 
 app.listen('5000')
+
+// http://localhost:5000/auth/google/callback
+
+// 313861558164-in75b3hf6j24j012lhr9689t7nkuv66k.apps.googleusercontent.com
+// 2x7qrtAXqS1Cfa2jwKMDuowW
