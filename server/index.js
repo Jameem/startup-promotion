284d15d15bf95173c423e6783c4359f778a6cf95
jameem <jameemkurikkal.mp@gmail.com>
2020-03-18 20:41:14 +0530

    file restructure

--- server/index.js
+++ server/index.js
@@ -1,33 +1,13 @@
 const express = require('express')
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-const keys = require('./config/keys')
+
+require('./services/passport')
 
 const app = express()
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientId,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken) => {
-    console.log(accessToken)
-}))
+require('./routes/authRoutes')(app)
 
-app.get('/auth/google', 
-    passport.authenticate('google', {
-        scope:['profile', 'email']
-    })
-)
-
-app.get('/auth/google/callback', passport.authenticate('google'))
-
-app.get('/', (req, res) => {
-    res.json({
-        hi: "hahaha"
-    })
-})
 const PORT = process.env.PORT || 5000
-app.listen(PORT)
-console.log("Running on port", PORT)
 
-// http://localhost:5000/auth/google/callback
+app.listen(PORT)
+
+console.log("Running on port", PORT)
