2d34b00ecb3804203fae3a28fcbd0a6c3e148d1c
jameem <jameemkurikkal.mp@gmail.com>
2020-03-20 08:16:35 +0530

    creating new user record

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,11 +1,26 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
 const keys = require('../config/keys')
+
+const User = mongoose.model('users')
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientId,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-}, (accessToken) => {
-    console.log(accessToken)
+}, (accessToken, refreshToken, profile, done) => {
+    User.findOne({
+        googleId: profile.id
+    }).then((existingUser) => {
+        if (existingUser) {
+            done(null, existingUser)
+        } else {
+            new User({
+                googleId: profile.id
+            }).save()
+        }
+    })
+
+
 }))
