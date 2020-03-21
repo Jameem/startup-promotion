56e7040be10c7dd2c71af591bad5c64b7b046cd7
jameem <jameemkurikkal.mp@gmail.com>
2020-03-21 08:09:26 +0530

    current user and logout routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -10,9 +10,11 @@ module.exports = app => {
 
     app.get('/auth/google/callback', passport.authenticate('google'))
 
-    app.get('/', (req, res) => {
-        res.json({
-            hi: "hahaha"
-        })
+    app.get('/api/logout', (req, res) => {
+        req.logout()
+    })
+
+    app.get('/api/user', (req, res) => {
+        res.send(req.user)
     })
 }
