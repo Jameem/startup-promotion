35ed51df689f0c4ec3d6e102c665d4025b7ba6c4
Jameem <jameemkurikkal.mp@gmail.com>
2020-05-14 08:31:38 +0530

    prettier formatting

--- server/index.js
+++ server/index.js
@@ -1,30 +1,30 @@
-const express = require('express')
-const mongoose= require('mongoose')
-const cookieSession = require('cookie-session')
-const passport = require('passport')
+const express = require("express")
+const mongoose = require("mongoose")
+const cookieSession = require("cookie-session")
+const passport = require("passport")
 
-const keys = require('./config/keys')
+const keys = require("./config/keys")
 
-require('./models/User')
-require('./services/passport')
+require("./models/User")
+require("./services/passport")
 
 const app = express()
 
 app.use(
-    cookieSession({
-        maxAge:30 * 24 *60 * 60 *1000,
-        keys: [keys.cookieKey]
-    })
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
 )
 
 app.use(passport.initialize())
 app.use(passport.session())
 
-require('./routes/authRoutes')(app)
+require("./routes/authRoutes")(app)
 
 mongoose.connect(keys.mongoURI, {
-    useNewUrlParser: true ,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
 
 const PORT = process.env.PORT || 5000
@@ -32,5 +32,3 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT)
 
 console.log("Running on port", PORT)
-
-
