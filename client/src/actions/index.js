4b3778265d4fe7507514e0d542ea7a1acaea87ea
Jameem <jameemkurikkal.mp@gmail.com>
2020-05-17 10:50:54 +0530

    fetchUser action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,5 +2,9 @@ import axios from "axios"
 import { FETCH_USER } from "./types"
 
 const fetchUser = () => {
-  axios.get("/api/user")
+  return (dispatch) => {
+    axios
+      .get("/api/user")
+      .then((res) => dispatch({ type: FETCH_USER, payload: res }))
+  }
 }
