b289242222e243ac2ab118859e797f24e45f3e75
Jameem <jameemkurikkal.mp@gmail.com>
2020-06-10 09:09:43 +0530

    error handling

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -7,20 +7,26 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate")
 const Survey = mongoose.model("surveys")
 module.exports = (app) => {
   app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
-    const { title, subject, body, recipients } = req.body
+    try {
+      const { title, subject, body, recipients } = req.body
 
-    const survey = new Survey({
-      title,
-      subject,
-      body,
-      recipients: recipients
-        .split(",")
-        .map((email) => ({ email: email.trim() })),
-      _user: req.user.id,
-      dateSent: Date.now(),
-    })
+      const survey = new Survey({
+        title,
+        subject,
+        body,
+        recipients: recipients
+          .split(",")
+          .map((email) => ({ email: email.trim() })),
+        _user: req.user.id,
+        dateSent: Date.now(),
+      })
 
-    const mailer = new Mailer(survey, surveyTemplate(survey))
-    mailer.send()
+      const mailer = new Mailer(survey, surveyTemplate(survey))
+
+      // console.log(survey)
+      mailer.send()
+    } catch (error) {
+      throw error
+    }
   })
 }
