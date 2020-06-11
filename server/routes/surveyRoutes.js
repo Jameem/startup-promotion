899627aec01851b2daf4532fc5b09b72c73e667c
Jameem <jameemkurikkal.mp@gmail.com>
2020-06-11 08:02:49 +0530

    Updating user after survey

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -6,7 +6,7 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate")
 
 const Survey = mongoose.model("surveys")
 module.exports = (app) => {
-  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     try {
       const { title, subject, body, recipients } = req.body
 
@@ -22,11 +22,16 @@ module.exports = (app) => {
       })
 
       const mailer = new Mailer(survey, surveyTemplate(survey))
+      await mailer.send()
 
-      // console.log(survey)
-      mailer.send()
+      await survey.save()
+
+      req.user.credits -= 1
+      const user = await req.user.save()
+
+      res.send(user)
     } catch (error) {
-      throw error
+      res.status(422).send(error)
     }
   })
 }
