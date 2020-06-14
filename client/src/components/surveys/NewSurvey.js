1140fdc1744332e506691554fd7610d463ce622c
Jameem <jameemkurikkal.mp@gmail.com>
2020-06-14 10:52:19 +0530

    clear form on new form button

--- client/src/components/surveys/NewSurvey.js
+++ client/src/components/surveys/NewSurvey.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import { reduxForm } from "redux-form"
 import SurveyForm from "./SurveyForm"
 import SurveyFormReview from "./SurveyFormReview"
 
@@ -26,4 +27,6 @@ class NewSurvey extends Component {
   }
 }
 
-export default NewSurvey
+export default reduxForm({
+  form: "surveyForm",
+})(NewSurvey)
